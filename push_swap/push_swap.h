1d6ff5771d64f99ee74e6a799966e7fb412d1a8e
Anton Skokov Belov <askokov-@c1r15s3.42lisboa.com>
2024-04-08 16:03:55 +0100

    08-04-2024

---
+++ push_swap/push_swap.h
@@ -0,0 +1,22 @@
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdbool.h>
+# include <limits.h>
+# include "./libft/libft.h"
+# include "./printf/printf.h"
+
+typedef struct s_stack_node
+{
+    int nbr;
+    int index;
+    int push_cost;
+    bool above_median;
+    bool cheapest;
+    struct s_stack_node *target_node;
+    struct s_stack_node *next;
+    struct s_stack_node *prev;
+} stack_node;
+
+#endif
